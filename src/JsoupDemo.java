cc1a958e959f7840f7e4da18b3d9c0c51d744f1c
Phani Choragudi <phani9895@gmail.com>
2019-05-16 00:32:36 -0400

    Added comments

--- src/JsoupDemo.java
+++ src/JsoupDemo.java
@@ -10,10 +10,11 @@ public class JsoupDemo {
 	public static void main(String[] args) {
 		
 		try {
-			
+			//'connect' initiates a connection to the specified url
+			//'userAgent' is used to mock a connection as if a browser is requesting it
 			Document doc = Jsoup.connect("https://www.imdb.com/list/ls053399670/").userAgent("Mozilla/17.0").get();
 		
-		
+			//Get all the elements with h3 that have name of the movies
 			Elements moviesList = doc.select("h3.lister-item-header");
 			
 			int i=0;
